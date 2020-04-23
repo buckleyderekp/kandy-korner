eb23e9fc64f294f6cea2316a74ca24c3ccacbd2f
buckleyderekp <buckleyderekp@gmail.com>
2020-04-23 16:08:14 -0500

    adding employees and employee form

--- src/components/KandyKorner.js
+++ src/components/KandyKorner.js
@@ -3,15 +3,20 @@ import LocationList from "./Location/LocationList"
 import { LocationProvider } from "./Location/LocationProvider"
 import ProductList from "./Products/ProductList"
 import { ProductProvider } from "./Products/ProductProvider"
+import { EmployeeProvider } from "./Employees/EmployeeProvider"
 import { ProductTypeProvider } from "./Products/ProductTypeProvider"
+import EmployeeList from "./Employees/EmployeeList"
 
 export default () => (
     <>
         <LocationProvider>
             <ProductProvider>
                 <ProductTypeProvider>
-                    <LocationList />
-                    <ProductList />
+                    <EmployeeProvider>
+                        <LocationList />
+                        <ProductList />
+                        <EmployeeList />
+                    </EmployeeProvider>
                 </ProductTypeProvider>
             </ProductProvider>
         </LocationProvider>
